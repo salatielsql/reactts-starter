4c706a476acff8035dc463b5c790fe44d94ae3c3
Salatiel Queiroz <salatielsqueiroz@gmail.com>
2021-11-19 01:21:28 -0300

    :tada:  first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import { Button } from '@components/Button'
+
+const App = (): JSX.Element => {
+  const [buttonText, setButtonText] = useState('Click me')
+
+  return (
+    <>
+      <h1>React Typescript Starter 3</h1>
+      <Button onClick={(): void => setButtonText('I was clicked!')}>{buttonText}</Button>
+    </>
+  )
+}
+
+export { App }
---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+/*
+  Avoid default imports due tree shaking. See: https://parceljs.org/languages/css/#tree-shaking
+*/
+import * as classnames from './Button.module.css'
+
+interface Props {
+  children: string | JSX.Element;
+  onClick: () => void;
+}
+
+const Button = ({ children, onClick }: Props): JSX.Element => (
+  <button type="button" onClick={onClick} className={classnames.button}>
+    {children}
+  </button>
+)
+
+/*
+  Avoid unnamed default exports. See: https://parceljs.org/recipes/react/#fast-refresh
+*/
+export { Button }
