6f2cb083995d7e4ec26b545d6c49899b4ec9a8d9
Salatiel Queiroz <salatielsqueiroz@gmail.com>
2022-01-06 12:27:23 -0300

    improv: update prettier and eslint to use personal config

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import { Button } from '@components/Button'
+import { Button } from "@components/Button";
 
 const App = (): JSX.Element => {
-  const [buttonText, setButtonText] = useState('Click me')
-
+  const [buttonText, setButtonText] = useState("Click me");
   return (
     <>
-      <h1>React Typescript Starter 3</h1>
-      <Button onClick={(): void => setButtonText('I was clicked!')}>{buttonText}</Button>
+      <h1>React Typescript Starter 2</h1>
+      <Button onClick={(): void => setButtonText("I was clicked!")}>
+        {buttonText}
+      </Button>
     </>
-  )
-}
+  );
+};
 
-export { App }
+export { App };
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 /*
   Avoid default imports due tree shaking. See: https://parceljs.org/languages/css/#tree-shaking
 */
-import * as classnames from './Button.module.css'
+import * as classnames from "./Button.module.css";
 
 interface Props {
   children: string | JSX.Element;
@@ -12,9 +12,9 @@ const Button = ({ children, onClick }: Props): JSX.Element => (
   <button type="button" onClick={onClick} className={classnames.button}>
     {children}
   </button>
-)
+);
 
 /*
   Avoid unnamed default exports. See: https://parceljs.org/recipes/react/#fast-refresh
 */
-export { Button }
+export { Button };
